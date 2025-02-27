4c3ea051219fa3a94d4f7c41001cb0460ae642a7
tapacuk <announcerf1@gmail.com>
2025-02-27 17:17:50 +0200

    fixed stuff

--- Lab2/C++/operations.h
+++ Lab2/C++/operations.h
@@ -4,20 +4,20 @@
 #include <string>
 class Operations
 {
-    private:
-        std::string text;
+private:
+    std::string text;
 
-    public:
-        Operations();
-        Operations(std::string input);
-        Operations(const Operations &copied);
+public:
+    Operations();
+    Operations(std::string input);
+    Operations(const Operations &copied);
 
-        std::string getText();
-        static bool isValidString(std::string input);
-        size_t stringLength();
-        std::string sortAsc();
+    std::string getText();
+    static bool isValidString(std::string input);
+    size_t stringLength();
+    std::string sortAsc();
 
-        ~Operations();
+    ~Operations();
 };
 
 #endif
