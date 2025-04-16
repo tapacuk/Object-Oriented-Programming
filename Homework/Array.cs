f1b5c8ae4e30a212781747e08551a8e4b3a757d4
tapacuk <announcerf1@gmail.com>
2025-04-16 13:16:56 +0300

    honework done

---
+++ Homework/Array.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Array
+{
+    int[,] arr;
+    private long prod;
+
+    public Array(int[,] input)
+    {
+        arr = input;
+        prod = CalculateProduct();
+    }
+
+    public double this[int row]
+    {
+        get
+        {
+            double sum = 0;
+            int cols = arr.GetLength(1);
+
+            for (int i = 0; i < cols; i++)
+            {
+                sum += arr[row, i];
+            }
+
+            return sum / cols;
+        }
+    }
+
+    public long Product
+    {
+        get { return prod; }
+    }
+
+    private long CalculateProduct()
+    {
+        long mul = 1;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                mul *= arr[i, j];
+            }
+        }
+        return mul;
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace HomeWork
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            int[,] arr = {
+                { 0, 7, 2, 0 },
+                { 2, 0, 1, 2 },
+                { 1, 9, 9, 9 }
+            };
+
+            Array arrObj = new Array(arr);
+
+            Console.WriteLine("avg of the first line: " + arrObj[0]);
+            Console.WriteLine("avg of the second line: " + arrObj[1]);
+            Console.WriteLine("avg of the third line: " + arrObj[2]);
+            Console.WriteLine("product of all elements: " + arrObj.Product);
+        }
+    }
+}
