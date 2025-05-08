d0c63ea2677d00ca411c5a899923dfb50a6a74b9
tapacuk <announcerf1@gmail.com>
2025-05-08 19:55:48 +0300

    minor edit

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,7 +6,6 @@ namespace HomeWork
     {
         static void Main(string[] args) 
         {
-            // .
             int[,] arr = {
                 { 0, 7, 2, 0 },
                 { 2, 0, 1, 2 },
