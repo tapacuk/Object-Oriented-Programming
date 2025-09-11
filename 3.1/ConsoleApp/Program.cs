b0aef056dcf557ca57a47b61c2c04d7fd03084d1
tapacuk <announcerf1@gmail.com>
2025-09-11 11:49:24 +0300

    finished 3.1

---
+++ 3.1/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileLogic;
+
+
+namespace StudList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager fm = new FileManager();
+            // fm.WriteFile(new string[] { $"{1}" });
+
+            ConsoleMenu cm = new ConsoleMenu(fm);
+            cm.ShowMenu();
+        }
+    }
+}
