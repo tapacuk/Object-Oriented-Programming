c741b8f5702debd61b9877ed39385abd5bed609d
tapacuk <announcerf1@gmail.com>
2025-09-11 11:58:14 +0300

    test

--- 3.1/Domain/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain
-{
-    public class Person
-    {
-        public string? name { get; set; }
-        public string? surname { get; set; }
-        public string? gender { get; set; }
-    }
-}
