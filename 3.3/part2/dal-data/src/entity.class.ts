0744698c3a5af169bbedf6b53582337377ec130e
announcerf <tasaneono22@gmail.com>
2025-10-14 21:02:46 +0300

    feat(lab3.3/part2): add json serializer

---
+++ 3.3/part2/dal-data/src/entity.class.ts
@@ -0,0 +1,15 @@
+export type Gender = 'male' | 'female';
+
+export class StudentEntity {
+  lastName: string | undefined;
+  firstName: string | undefined;
+  course: number | undefined;
+  studentId: string | undefined;
+  gender: Gender | undefined;
+  city: string | undefined;
+  recordBookNumber: string | undefined;
+
+  constructor(init?: Partial<StudentEntity>) {
+    Object.assign(this, init);
+  }
+}
---
+++ 3.3/part2/dal-data/src/serializers/jsonSerializer.ts
@@ -0,0 +1,22 @@
+import { StudentEntity } from '../entity.class';
+import type { IDataProvider } from './IDataProvider';
+import { promises as fs } from 'fs';
+
+export class JSONProvider implements IDataProvider<StudentEntity> {
+  async read(filePath: string): Promise<StudentEntity[]> {
+    const parsed = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+    return parsed.map((o: any) => Object.assign(new StudentEntity(), o));
+  }
+
+  async write(filePath: string, items: StudentEntity[]): Promise<void> {
+    await fs.writeFile(filePath, JSON.stringify(items, null, 2), 'utf-8');
+  }
+
+  async create(filePath: string): Promise<void> {
+    await fs.writeFile(filePath, '[]', 'utf-8');
+  }
+
+  async deleteFile(filePath: string): Promise<void> {
+    await fs.unlink(filePath);
+  }
+}
