d2a291a0f6b5509eccd730ab401ba58ba4637313
announcerf <tasaneono22@gmail.com>
2025-10-22 23:03:48 +0300

    rework(3.4): edit mobile connection class and add fix index

--- 3.4/src/index.ts
+++ 3.4/src/index.ts
@@ -1,26 +1,30 @@
 import { MobileConnection } from './mobileConnection.js';
 import { EventArgs } from './eventArgs.js';
+import type { EventHandler } from './eventHandler.js';
 
 export class App {
   private connection: MobileConnection;
 
   constructor() {
     this.connection = new MobileConnection(100, 20);
-    this.connection.onBalanceLow(this.handleBalanceLow);
+
+    const handler: EventHandler<EventArgs> = this.handleBalanceLow;
+    this.connection.onBalanceLow(handler);
   }
 
-  private handleBalanceLow(sender: any, e: EventArgs) {
-    console.log(`Подія: ${e.message}`);
-    // console.log(`Ініціатор події:`, sender);
-  }
+  private handleBalanceLow = (sender: any, e: EventArgs) => {
+    console.log(`----- Подія: ${e.message}`);
+    console.log(`• Ініціатор події:`, sender.constructor.name);
+  };
 
-  public run() {
+  public run = () => {
+    console.log('=== Початок симуляції мобільного зв’язку ===');
     this.connection.useService(30);
-    this.connection.topUp(20);
     this.connection.useService(50);
+    this.connection.topUp(10);
     this.connection.useService(10);
     this.connection.useService(5);
-  }
+  };
 }
 
 const app = new App();
--- 3.4/src/mobileConnection.ts
+++ 3.4/src/mobileConnection.ts
@@ -11,42 +11,42 @@ export class MobileConnection {
     this.minBalance = minBalance;
   }
 
-  public onBalanceLow(handler: EventHandler<EventArgs>) {
+  public onBalanceLow = (handler: EventHandler<EventArgs>) => {
     this.balanceLowHandlers.push(handler);
-  }
+  };
 
-  protected raiseBalanceLow(e: EventArgs) {
+  protected raiseBalanceLow = (e: EventArgs) => {
     for (const handler of this.balanceLowHandlers) {
       handler(this, e); // sender = this
     }
-  }
+  };
 
-  public topUp(amount: number) {
+  public topUp = (amount: number) => {
     this.balance += amount;
 
     console.log(
       `Баланс поповнено на ${amount}. Поточний баланс: ${this.balance}`
     );
-  }
+  };
 
-  public useService(cost: number) {
+  public useService = (cost: number) => {
     if (this.balance >= cost) {
       this.balance -= cost;
       console.log(`Використано ${cost}. Залишок: ${this.balance}`);
 
-      if (this.balance <= this.minBalance) {
+      if (this.balance < this.minBalance) {
         const args = new EventArgs(
           this.balance,
-          `Баланс знизився до ${this.balance} грн (мінімум — ${this.minBalance})!`
+          `Баланс нижче ${this.minBalance}! Поточний баланс: ${this.balance}.`
         );
         this.raiseBalanceLow(args);
       }
     } else {
       console.log(`Недостатньо коштів для операції.`);
     }
-  }
+  };
 
-  public getBalance() {
+  public getBalance = () => {
     return this.balance;
-  }
+  };
 }
