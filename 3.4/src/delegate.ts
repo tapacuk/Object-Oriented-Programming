b44acc5946201f56cfb3a5a57bc7a5a5da9dcffc
announcerf <tasaneono22@gmail.com>
2025-10-22 23:27:46 +0300

    feat(3.4): add anonymus function for matrix diagonal finder

--- 3.4/src/delegate.ts
+++ 3.4/src/delegate.ts
@@ -3,3 +3,7 @@ export type MatrixDiagonal = (matrix: number[][]) => number[];
 export const getMainDiagonal: MatrixDiagonal = (matrix) => {
   return matrix.map((row, i) => row[i]!);
 };
+
+const getMainDiagonalAnonymus: MatrixDiagonal = function (matrix) {
+  return matrix.map((row, i) => row[i]!);
+};
