7c1cc66cf4ea97a10bbe4d10bcd0d8435fd384ba
announcerf <tasaneono22@gmail.com>
2025-11-08 14:13:32 +0200

    fix(): fix error messages

--- 3.5/bll-service/src/lib/studentModel.ts
+++ 3.5/bll-service/src/lib/studentModel.ts
@@ -1,13 +1,13 @@
-type Gender = 'male' | 'female';
+import type { Gender } from './gender.type';
 
 export class StudentModel {
   constructor(
-    public lastName: string | undefined,
-    public firstName: string | undefined,
-    public course: number | undefined,
-    public studentId: string | undefined,
-    public gender: Gender | undefined,
-    public city: string | undefined,
-    public recordBookNumber: string | undefined
+    public lastName: string,
+    public firstName: string,
+    public course: number,
+    public studentId: string,
+    public gender: Gender,
+    public city: string,
+    public recordBookNumber: string
   ) {}
 }
--- 3.5/bll-service/src/studentService.ts
+++ 3.5/bll-service/src/studentService.ts
@@ -2,6 +2,7 @@ import { StudentNotFoundException } from './lib/studentExceptions';
 import type { IEntityContext } from './lib/iEntityContext.interface';
 import type { StudentModel } from './lib/studentModel';
 import type { IStudentService } from './lib/iStudentService.interface';
+import type { Gender } from './lib/gender.type';
 
 export class StudentService implements IStudentService {
   private students: StudentModel[] = [];
@@ -13,9 +14,39 @@ export class StudentService implements IStudentService {
   }
 
   addStudent(student: StudentModel): void {
-    if (!student.firstName || !student.lastName) {
-      throw new Error('Некоректні дані студента.');
+    if (
+      !student.firstName ||
+      !student.lastName ||
+      student.firstName.trim() === '' ||
+      student.lastName.trim() === ''
+    ) {
+      throw new Error(
+        "Некоректні дані студента: Ім'я та Прізвище є обов'язковими полями"
+      );
     }
+
+    if (this.students.find((s) => s.studentId === student.studentId)) {
+      throw new Error(`Студент з ID ${student.studentId} вже існує.`);
+    }
+
+    if (!student.city || student.city.trim() === '') {
+      student.city = 'Unknown';
+    }
+
+    if (!student.recordBookNumber || student.recordBookNumber.trim() === '') {
+      student.recordBookNumber = 'N/A';
+    }
+
+    if (!student.course || student.course < 1 || student.course > 6) {
+      throw new Error('Некоректний курс студента: Допустимі значення: 1-6.');
+    }
+
+    if (student.gender !== 'male' && student.gender !== 'female') {
+      throw new Error(
+        "Некоректна стать студента: Допустимі значення: 'male', 'female '"
+      );
+    }
+
     this.students.push(student);
   }
 
@@ -37,7 +68,7 @@ export class StudentService implements IStudentService {
       (s) =>
         s.gender === 'female' &&
         s.course === 5 &&
-        s.city!.toLowerCase() === 'київ'
+        s.city!.toLowerCase() === 'kyiv'
     ).length;
   }
 
@@ -55,7 +86,7 @@ export class StudentService implements IStudentService {
       throw new StudentNotFoundException(`Студента з ID ${id} не знайдено.`);
 
     const city = (student.city ?? '').trim();
-    if (city !== 'Kyiv') {
+    if (city.toLowerCase() !== 'kyiv') {
       student.city = 'Kyiv (In hostel)';
     } else {
       if (!city.includes('(In hostel)')) {
