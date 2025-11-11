a82ba308d4a2ca687ba1badb4784ed5cf7d29c40
announcerf <tasaneono22@gmail.com>
2025-11-11 18:15:39 +0200

    fix(core): fix configs

--- 3.5/jest.config.js
+++ 3.5/jest.config.js
@@ -1,11 +1,18 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 export default {
-  testEnvironment: "node",
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
   },
+  moduleNameMapper: {
+    '^@bll-service/(.*)$': '<rootDir>/bll-service/src/$1',
+  },
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    './bll-service/src/**/*.ts',
+    '!./bll-service/src/lib/**',
+  ],
+  coverageDirectory: '<rootDir>/bll-service/tests/coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
 };
